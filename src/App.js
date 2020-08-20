1967b6ccad4e418c74c236c129ab92c8d91f661d
lilianyangc <mlilianyang@gmail.com>
2020-08-20 11:54:41 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -4,12 +4,8 @@ import Layout from './hoc/Layout/Layout';
 
 function App() {
   return (
-    // <div className="App">
-    //   <header className="App-header">
-    //    App.js
-    //   </header>
-    // </div>
-           <Layout></Layout>
+ 
+    <Layout></Layout>
 
   );
 }
